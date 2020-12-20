b123d34e79874b6933d6c205e38ef0f22c8daa18
Thanapong Angkha <lindstorm76@outlook.com>
2020-12-20 16:38:01 +0700

    Changes made to this commit: 	Refactored the code. Removed duplicated/verbose code.

--- src/App.js
+++ src/App.js
@@ -13,19 +13,85 @@ class App extends React.Component {
       heading: "Dota 2 Heroes",
       atkType: "Hover on any hero to reveal their roles...",
       desc: null,
-      strCards: null,
-      agiCards: null,
-      intCards: null,
       role: null,
       attack_type: null,
       name: null
     }
   }
 
-  filter = (role, attack_type, name) => {
+  componentDidMount = () => {
+    fetch("https://api.opendota.com/api/heroes")
+    .then(res => res.json())
+    .then(heroes => {
+      this.setState({ heroes: heroes.sort((a, b) => a.name > b.name ? 1 : -1) })
+    })
+  }
+  
+  showDetail = e => {
+    const hoveredHero = this.state.heroes.find(hero => hero.id === +e.target.dataset.id)
+    this.setState({
+      heading: hoveredHero.localized_name,
+      atkType: hoveredHero.attack_type,
+      desc: hoveredHero.roles.reduce((acc, cur) => acc + " - " + cur, "")
+    })
+  }
+
+  capitalize = word => (
+    word[0].toUpperCase() + word.substring(1).toLowerCase()
+  )
+  
+  filterRole = e => {
+    const target = this.capitalize(e.target.value)
+    this.setState({
+      role: target === "By role" || target === "All" ? null : target
+    })
+  }
+
+  filterAttackType = e => {
+    const type = this.capitalize(e.target.value)
+    this.setState({
+      attack_type: type === "By attack type" || type === "All" ? null : type
+    })
+  }
+
+  filterName = e => {
+    this.setState({
+      name: e.target.value === "HERO NAME" ? null : e.target.value
+    })
+  }
+
+  render() {
+
+    const loadingOptions = {
+      loop: true,
+      autoplay: true,
+      animationData: loadingData.default,
+      rendererSettings: {
+        preserveAspectRatio: "xMidYMid slice"
+      }
+    }
+
+    if (!this.state.heroes) {
+      return(
+        <FadeIn>
+          <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            width: "99vw",
+            height: "95vh" }}
+          >
+            <Lottie options={loadingOptions} height={200} width={200} />
+          </div>
+        </FadeIn>
+      )
+    }
+
     const strCards = [], agiCards = [], intCards = []
     this.state.heroes.forEach(hero => {
       let validRole = true, validType = true, validName = true
+      const { role, attack_type, name } = this.state
       if (role !== null) {
         validRole = hero.roles.includes(role)
       }
@@ -74,73 +140,12 @@ class App extends React.Component {
         )
       }
     })
-    this.setState({ strCards, agiCards, intCards })
-  }
-
-  componentDidMount = () => {
-    fetch("https://api.opendota.com/api/heroes")
-    .then(res => res.json())
-    .then(heroes => {
-      this.setState({ heroes: heroes.sort((a, b) => a.name > b.name ? 1 : -1) })
-      this.filter(null, null, null)
-    })
-  }
-  
-  showDetail = e => {
-    const hoveredHero = this.state.heroes.find(hero => hero.id === +e.target.dataset.id)
-    this.setState({
-      heading: hoveredHero.localized_name,
-      atkType: hoveredHero.attack_type,
-      desc: hoveredHero.roles.reduce((acc, cur) => acc + " - " + cur, "")
-    })
-  }
-  
-  filterRole = e => {
-    const target = e.target.value.substring(0, 1).toUpperCase() + e.target.value.substring(1).toLowerCase()
-    const result = target === "By role" || target === "All" ? null : target  
-    this.setState({ role: result })
-    this.filter(result, this.state.attack_type, this.state.name)
-  }
-
-  filterAttackType = e => {
-    const type = e.target.value.substring(0, 1).toUpperCase() + e.target.value.substring(1).toLowerCase()
-    const result = type === "By attack type" || type === "All" ? null : type
-    this.setState({ attack_type: result })
-    this.filter(this.state.role, result, this.state.name)
-  }
-
-  filterName = e => {
-    const result = e.target.value === "HERO NAME" ? null : e.target.value
-    this.setState({ name: result })
-    this.filter(this.state.role, this.state.attack_type, result)
-  }
-
-  render() {
-
-    const loadingOption = {
-      loop: true,
-      autoplay: true,
-      animationData: loadingData.default,
-      rendererSettings: {
-        preserveAspectRatio: "xMidYMid slice"
-      }
-    }
-
-    if (this.state.strCards === null || this.state.agiCards === null || this.state.intCards === null) {
-      return(
-        <FadeIn>
-          <div style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "99vw", height: "95vh" }}>
-            <Lottie options={loadingOption} height={200} width={200} />
-          </div>
-        </FadeIn>
-      )
-    }
 
     const names = this.state.heroes.map(hero => hero.localized_name).sort().map(name => {
       return <option key={"hero-" + name}>{name}</option>
     })
     
-    return(
+    return (
       <div className="center-container">
         <div className="heading-container">
           <h1 className="heading">{this.state.heading}</h1>
@@ -155,15 +160,15 @@ class App extends React.Component {
           <div className="container">
             <h2 className="attr" style={{color: "#EF4444"}}>strength</h2>
             <div className="hero-container">
-              {this.state.strCards}
+              {strCards}
             </div>
             <h2 className="attr" style={{color: "#10B981"}}>agility</h2>
             <div className="hero-container">
-              {this.state.agiCards}
+              {agiCards}
             </div>
             <h2 className="attr" style={{color: "#3B82F6"}}>intelligence</h2>
             <div className="hero-container">
-              {this.state.intCards}
+              {intCards}
             </div>
           </div>
         </FadeIn>
@@ -172,4 +177,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default App
