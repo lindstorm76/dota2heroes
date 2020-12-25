471054bcef2011d4b6c9b6c30ab73640a23b97a3
Thanapong Angkha <lindstorm76@outlook.com>
2020-12-25 23:11:42 +0700

    Changes made to this commit: modified:   src/App.tsx 	Removed deafult importing of App.tsc component. modified:   src/index.tsx 	Changed from class component to functional component. Used 	useEffect instead of componentDidMount. Used Animation component 	instead of manually creating animation option and load it into 	Lottie component. Got rid of "this". modified:   src/style.css 	Added horizontal padding to the hero detail page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,194 +1,148 @@
-import React, { ChangeEvent } from "react"
+import React, { ChangeEvent, useEffect, useState } from "react"
 import { HeroCard } from "./components/HeroCard"
-import loadingData from "./loading.json"
+import loading from "./loading.json"
 import FadeIn from "react-fade-in"
-import Lottie from "react-lottie"
 import { FilterOption } from "./components/FilterOption"
+import { Animation } from "./components/Animation"
 
-class App extends React.Component<{}> {
+export const App: React.FC = (): JSX.Element => {
+  const [heading, setHeading] = useState("Dota 2 Heroes")
+  const [role, setRole] = useState(null)
+  const [attackType, setAttackType] = useState(null)
+  const [name, setName] = useState(null)
+  const [heroes, setHeroes] = useState(null)
 
-  state: {
-    heading: string,
-    atkType: string,
-    desc: string | null,
-    role: string | null,
-    attack_type: string | null,
-    name: string | null,
-    heroes: Array<Object> | null
-  } = {
-    heading: "Dota 2 Heroes",
-    atkType: "Hover on any hero to reveal their roles...",
-    desc: null,
-    role: null,
-    attack_type: null,
-    name: null,
-    heroes: null
+  useEffect((): void => {
+    (async (): Promise<void> => {
+      const res = await fetch("https://api.opendota.com/api/heroes")
+      const heroes = await res.json()
+      heroes.sort((a: any, b: any) => a.name > b.name ? 1 : -1)
+      setHeroes(heroes)
+    })()
+  }, [])
+
+  const showDetail = (e: any): void => {
+    const hoveredHeroName: string = heroes.find((hero: any) => (
+      hero.id === +e.target.dataset.id
+    )).localized_name
+    setHeading(hoveredHeroName)
   }
 
-  componentDidMount = (): void => {
-    fetch("https://api.opendota.com/api/heroes")
-    .then(res => res.json())
-    .then(heroes => {
-      this.setState({ heroes: heroes.sort((a: any, b: any) => a.name > b.name ? 1 : -1) })
-    })
-  }
-  
-  showDetail = (e: any): void => {
-    const hoveredHero: any = this.state.heroes.find((hero: any) => hero.id === +e.target.dataset.id)
-    this.setState({
-      heading: hoveredHero.localized_name,
-      atkType: hoveredHero.attack_type,
-      desc: hoveredHero.roles.reduce((acc: any, cur: any) => acc + " - " + cur, "")
-    })
-  }
-
-  capitalize = (word: string): string => (
+  const capitalize = (word: string): string => (
     word[0].toUpperCase() + word.substring(1).toLowerCase()
   )
-  
+
   // This method is only invoked if a select element changes.
   // Each event that is invoked doesn't have the same type of event.
-  filterRole = (e: ChangeEvent<HTMLSelectElement>): void => {
-    const target = this.capitalize(e.target.value)
-    this.setState({
-      role: target === "By role" || target === "All" ? null : target
-    })
+  const filterRole = (e: ChangeEvent<HTMLSelectElement>): void => {
+    const target = capitalize(e.target.value)
+    setRole(target === "By role" || target === "All" ? null : target)
   }
 
-  filterAttackType = (e: ChangeEvent<HTMLSelectElement>): void => {
-    const type = this.capitalize(e.target.value)
-    this.setState({
-      attack_type: type === "By attack type" || type === "All" ? null : type
-    })
+  const filterAttackType = (e: ChangeEvent<HTMLSelectElement>): void => {
+    const type = capitalize(e.target.value)
+    setAttackType(type === "By attack type" || type === "All" ? null : type)
   }
 
-  filterName = (e: ChangeEvent<HTMLSelectElement>): void => {
-    this.setState({
-      name: e.target.value === "HERO NAME" ? null : e.target.value
-    })
+  const filterName = (e: ChangeEvent<HTMLSelectElement>): void => {
+    setName(e.target.value === "HERO NAME" ? null : e.target.value)
   }
 
-  render(): JSX.Element {
-
-    const loadingOptions: any = {
-      loop: true,
-      autoplay: true,
-      animationData: loadingData,
-      rendererSettings: {
-        preserveAspectRatio: "xMidYMid slice"
-      }
-    }
-
-    if (!this.state.heroes) {
-      return(
-        <FadeIn>
-          <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            width: "99vw",
-            height: "95vh" }}
-          >
-            <Lottie options={loadingOptions} height={200} width={200} />
-          </div>
-        </FadeIn>
-      )
-    }
-
-    const strCards: Array<JSX.Element> = [],
-          agiCards: Array<JSX.Element> = [],
-          intCards: Array<JSX.Element> = []
-    this.state.heroes.forEach((hero: any) => {
-      let validRole: boolean = true,
-          validType: boolean = true,
-          validName: boolean = true
-      const { role, attack_type, name } = this.state
-      if (role !== null) {
-        validRole = hero.roles.includes(role)
-      }
-      if (attack_type !== null) {
-        validType = hero.attack_type === attack_type
-      }
-      if (name !== null) {
-        validName = hero.localized_name === name
-      }
-      const validity: boolean = validRole && validType && validName
-      if (hero.primary_attr === "str") {
-        strCards.push(
-          <HeroCard
-            id={hero.id}
-            key={`hero-${hero.id}`}
-            mouseOver={this.showDetail}
-            attr={hero.primary_attr}
-            localized_name={hero.localized_name}
-            name={hero.name.split("_dota_hero_")[1]}
-            isValid={validity}
-          />
-        )
-      } else if (hero.primary_attr === "agi") {
-        agiCards.push(
-          <HeroCard
-            id={hero.id}
-            key={`hero-${hero.id}`}
-            mouseOver={this.showDetail}
-            attr={hero.primary_attr}
-            localized_name={hero.localized_name}
-            name={hero.name.split("_dota_hero_")[1]}
-            isValid={validity}
-          />
-        )
-      } else if (hero.primary_attr === "int") {
-        intCards.push(
-          <HeroCard
-            id={hero.id}
-            key={`hero-${hero.id}`}
-            mouseOver={this.showDetail}
-            attr={hero.primary_attr}
-            localized_name={hero.localized_name}
-            name={hero.name.split("_dota_hero_")[1]}
-            isValid={validity}
-          />
-        )
-      }
-    })
-
-    const names: Array<JSX.Element> = this.state.heroes.map((hero: any) => (
-      hero.localized_name
-    )).sort().map((name: string) => (
-      <option key={"hero-" + name}>{name}</option>
-    ))
-    
+  if (!heroes) {
     return (
-      <div className="center-container">
-        <div className="heading-container">
-          <h1 className="heading">{this.state.heading}</h1>
-        </div>
-        <FilterOption
-          filterRole={this.filterRole}
-          filterAttackType={this.filterAttackType}
-          filterName={this.filterName}
-          names={names}
-        />
-        <FadeIn>
-          <div className="container">
-            <h2 className="attr" style={{color: "#EF4444"}}>strength</h2>
-            <div className="hero-container">
-              {strCards}
-            </div>
-            <h2 className="attr" style={{color: "#10B981"}}>agility</h2>
-            <div className="hero-container">
-              {agiCards}
-            </div>
-            <h2 className="attr" style={{color: "#3B82F6"}}>intelligence</h2>
-            <div className="hero-container">
-              {intCards}
-            </div>
-          </div>
-        </FadeIn>
-      </div>
+      <Animation animation={loading} width={200} height={200} />
     )
   }
-}
 
-export default App
+  const strCards: Array<JSX.Element> = [],
+        agiCards: Array<JSX.Element> = [],
+        intCards: Array<JSX.Element> = []
+  heroes.forEach((hero: any) => {
+    let validRole: boolean = true,
+        validType: boolean = true,
+        validName: boolean = true
+    if (role !== null) {
+      validRole = hero.roles.includes(role)
+    }
+    if (attackType !== null) {
+      validType = hero.attack_type === attackType
+    }
+    if (name !== null) {
+      validName = hero.localized_name === name
+    }
+    const validity: boolean = validRole && validType && validName
+    if (hero.primary_attr === "str") {
+      strCards.push(
+        <HeroCard
+          id={hero.id}
+          key={`hero-${hero.id}`}
+          mouseOver={showDetail}
+          attr={hero.primary_attr}
+          localized_name={hero.localized_name}
+          name={hero.name.split("_dota_hero_")[1]}
+          isValid={validity}
+        />
+      )
+    } else if (hero.primary_attr === "agi") {
+      agiCards.push(
+        <HeroCard
+          id={hero.id}
+          key={`hero-${hero.id}`}
+          mouseOver={showDetail}
+          attr={hero.primary_attr}
+          localized_name={hero.localized_name}
+          name={hero.name.split("_dota_hero_")[1]}
+          isValid={validity}
+        />
+      )
+    } else if (hero.primary_attr === "int") {
+      intCards.push(
+        <HeroCard
+          id={hero.id}
+          key={`hero-${hero.id}`}
+          mouseOver={showDetail}
+          attr={hero.primary_attr}
+          localized_name={hero.localized_name}
+          name={hero.name.split("_dota_hero_")[1]}
+          isValid={validity}
+        />
+      )
+    }
+  })
+
+  const names: Array<JSX.Element> = heroes.map((hero: any) => (
+    hero.localized_name
+  )).sort().map((name: string) => (
+    <option key={"hero-" + name}>{name}</option>
+  ))
+
+  return (
+    <div className="center-container">
+      <div className="heading-container">
+        <h1 className="heading">{heading}</h1>
+      </div>
+      <FilterOption
+        filterRole={filterRole}
+        filterAttackType={filterAttackType}
+        filterName={filterName}
+        names={names}
+      />
+      <FadeIn>
+        <div className="container">
+          <h2 className="attr" style={{color: "#EF4444"}}>strength</h2>
+          <div className="hero-container">
+            {strCards}
+          </div>
+          <h2 className="attr" style={{color: "#10B981"}}>agility</h2>
+          <div className="hero-container">
+            {agiCards}
+          </div>
+          <h2 className="attr" style={{color: "#3B82F6"}}>intelligence</h2>
+          <div className="hero-container">
+            {intCards}
+          </div>
+        </div>
+      </FadeIn>
+    </div>
+  )
+}
