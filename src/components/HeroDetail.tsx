ec86c5a7123125565033a355984a74413ed4d240
Thanapong Angkha <lindstorm76@outlook.com>
2020-12-25 22:36:35 +0700

    Changes made to this commit: modified:   src/components/HeroDetail.tsx 	Changed from class component to functional component. modified:   src/index.tsx 	Removed default importing. modified:   src/style.css 	Added style for animaiton container.

--- src/components/HeroDetail.tsx
+++ src/components/HeroDetail.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import FadeIn from "react-fade-in"
 import Lottie from "react-lottie"
 import loadingData from "../loading.json"
@@ -8,100 +8,92 @@ interface HeroDetailProps {
   match: any
 }
 
-class HeroDetail extends React.Component<HeroDetailProps> {
-
-  state: {
-    hero: any | null
-  } = {
-    hero: null
-  }
-
-  componentDidMount = (): void => {
+export const HeroDetail: React.FC<HeroDetailProps> = ({
+  match
+}): JSX.Element => {
+  const [hero, setHero] = useState(null)
+  useEffect((): void => {
     fetch("https://api.opendota.com/api/heroes")
     .then(res => res.json())
     .then(heroes => {
-      this.setState({ hero: heroes.find((hero: any) => hero.localized_name === this.props.match.params.name) })
+      const target = heroes.find((hero: any) => hero.localized_name === match.params.name)
+      setHero(target)
     })
+  }, [match]) 
+
+  const loadingOption: any = {
+    loop: true,
+    autoplay: true,
+    animationData: loadingData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
   }
 
-  render(): JSX.Element {
-
-    const loadingOption: any = {
-      loop: true,
-      autoplay: true,
-      animationData: loadingData,
-      rendererSettings: {
-        preserveAspectRatio: "xMidYMid slice"
-      }
+  const notfoundOption: any = {
+    loop: true,
+    autoplay: true,
+    animationData: notfound,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
     }
+  }
 
-    const notfoundOption: any = {
-      loop: true,
-      autoplay: true,
-      animationData: notfound,
-      rendererSettings: {
-        preserveAspectRatio: "xMidYMid slice"
-      }
-    }
-
-    if (this.state.hero === null) {
-      return(
-        <FadeIn>
-          <div style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "99vw", height: "95vh" }}>
-            <Lottie options={loadingOption} height={200} width={200} />
-          </div>
-        </FadeIn>
-      )
-    }
-
-    if (this.state.hero === undefined) {
-      return(
-        <FadeIn>
-          <div style={{ display: "flex", justifyContent: "center", alignItems: "center", width: "99vw", height: "95vh" }}>
-            <Lottie options={notfoundOption} height={400} width={400} />
-          </div>
-        </FadeIn>
-      )
-    }
-
-    const roles: string = this.state.hero.roles.reduce((acc: string, cur: string) => acc + " - " + cur, "")
-    let color: string, attr: string, src: string
-    const { primaryAttr } = this.state.hero.primary_attr
-    if (primaryAttr === "str") {
-      color = "#EF4444"
-      attr = "strength"
-      src = "https://static.wikia.nocookie.net/dota2_gamepedia/images/7/7a/Strength_attribute_symbol.png"
-    } else if (primaryAttr === "agi") {
-      color = "#10B981"
-      attr = "agility"
-      src = "https://static.wikia.nocookie.net/dota2_gamepedia/images/2/2d/Agility_attribute_symbol.png"
-    } else {
-      color = "#3B82F6"
-      attr = "inteligence"
-      src = "https://static.wikia.nocookie.net/dota2_gamepedia/images/5/56/Intelligence_attribute_symbol.png"
-    }
-
-    return (
-      <div className="center-container">
-        <h1 className="heading">{this.state.hero.localized_name}</h1>
-        <FadeIn>
-          <div className="hero-image-container">
-            <img
-              alt={this.state.hero.name}
-              src={`https://cdn.dota2.com/apps/dota2/images/heroes/${this.state.hero.name.split("_dota_hero_")[1]}_full.png`}
-              style={{width: "20rem"}}
-            />
-          </div>
-          <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
-          <img alt={this.state.hero.primary_attr} className="hero-attribute" src={src} />
-          <h2 className="attr" style={{color}}>{attr}</h2>
-          </div>
-          <h3 className="sub-heading">{this.state.hero.attack_type}<span style={{color: "gray"}}>{roles}</span></h3>
-        </FadeIn>
-      </div>
-      
+  if (hero === null) {
+    return(
+      <FadeIn>
+        <div className="animation-container">
+          <Lottie options={loadingOption} height={200} width={200} />
+        </div>
+      </FadeIn>
     )
   }
-}
 
-export default HeroDetail
+  if (hero === undefined) {
+    return(
+      <FadeIn>
+        <div className="animation-container">
+          <Lottie options={notfoundOption} height={400} width={400} />
+        </div>
+      </FadeIn>
+    )
+  }
+
+  const roles: string = hero.roles.reduce((acc: string, cur: string) => acc + " - " + cur, "")
+  let color: string, attr: string, src: string
+  const { primaryAttr } = hero
+  if (primaryAttr === "str") {
+    color = "#EF4444"
+    attr = "strength"
+    src = "https://static.wikia.nocookie.net/dota2_gamepedia/images/7/7a/Strength_attribute_symbol.png"
+  } else if (primaryAttr === "agi") {
+    color = "#10B981"
+    attr = "agility"
+    src = "https://static.wikia.nocookie.net/dota2_gamepedia/images/2/2d/Agility_attribute_symbol.png"
+  } else {
+    color = "#3B82F6"
+    attr = "inteligence"
+    src = "https://static.wikia.nocookie.net/dota2_gamepedia/images/5/56/Intelligence_attribute_symbol.png"
+  }
+
+  return (
+    <div className="center-container">
+      <h1 className="heading">{hero.localized_name}</h1>
+      <FadeIn>
+        <div className="hero-image-container">
+          <img
+            alt={hero.name}
+            src={`https://cdn.dota2.com/apps/dota2/images/heroes/${hero.name.split("_dota_hero_")[1]}_full.png`}
+            style={{width: "20rem"}}
+          />
+        </div>
+        <div style={{display: "flex", alignItems: "center", justifyContent: "center"}}>
+        <img alt={hero.primary_attr} className="hero-attribute" src={src} />
+        <h2 className="attr" style={{color}}>{attr}</h2>
+        </div>
+        <h3 className="sub-heading">{hero.attack_type}<span style={{color: "gray"}}>{roles}</span></h3>
+      </FadeIn>
+    </div>
+    
+  )
+}
